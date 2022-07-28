6d8f742f263a2cf670d5e20f59f56492dfb379a4
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-29 01:22:39 +0530

    create Employee model

---
+++ CoreData4/Models/Employee.swift
@@ -0,0 +1,16 @@
+//
+//  Employee.swift
+//  CoreData4
+//
+//  Created by Abhinay Pratap on 29/07/22.
+//
+
+import Foundation
+
+
+struct Employee {
+    var name: String?
+    var email: String?
+    var id: UUID
+    var profilePicture: Data?
+}
---
+++ CoreData4/Repositories/EmployeeDataRepository.swift
@@ -0,0 +1,8 @@
+//
+//  EmployeeDataRepository.swift
+//  CoreData4
+//
+//  Created by Abhinay Pratap on 29/07/22.
+//
+
+import Foundation
