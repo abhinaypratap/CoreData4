ce4d6566b3b765957d47a3de6b024c680ff2f0dd
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-29 01:26:25 +0530

    add necessary protocols and structures

--- CoreData4/Repositories/EmployeeDataRepository.swift
+++ CoreData4/Repositories/EmployeeDataRepository.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import CoreData
+
+protocol EmployeeRepository {
+    func create(employee: Employee)
+    func getAll() -> [Employee]?
+    func get(byIdentifier: UUID) -> Employee?
+    func update(employee: Employee)
+    func delete(employee: Employee)
+}
+
+struct EmployeeDataRepository: EmployeeRepository {
+    func create(employee: Employee) {
+        <#code#>
+    }
+    
+    func getAll() -> [Employee]? {
+        <#code#>
+    }
+    func get(byIdentifier: UUID) -> Employee? {
+        <#code#>
+    }
+    
+    func update(employee: Employee) {
+        <#code#>
+    }
+    
+    func delete(employee: Employee) {
+        <#code#>
+    }
+}
