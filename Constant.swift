a11b2fd6dbb375ffbabf972687d4c4e9061b8fbf
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-29 14:09:14 +0530

    create constants file

---
+++ Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  CoreData4
+//
+//  Created by Abhinay Pratap on 29/07/22.
+//
+
+import Foundation
+
+
+struct K {
+    static let employeeListVC = "EmployeeListVC"
+    static let employeeDetailVC = "EmployeeDetailVC"
+}
