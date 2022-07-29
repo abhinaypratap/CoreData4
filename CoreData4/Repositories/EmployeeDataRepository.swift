be62b9b1d74eaaf50e8cb5c66377abba86de52e1
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-29 10:09:07 +0530

    create, getAll and get functions in cdrepository

--- CoreData4/Repositories/EmployeeDataRepository.swift
+++ CoreData4/Repositories/EmployeeDataRepository.swift
@@ -11,21 +11,44 @@ import CoreData
 protocol EmployeeRepository {
     func create(employee: Employee)
     func getAll() -> [Employee]?
-    func get(byIdentifier: UUID) -> Employee?
+    func get(byIdentifier id: UUID) -> Employee?
     func update(employee: Employee)
     func delete(employee: Employee)
 }
 
 struct EmployeeDataRepository: EmployeeRepository {
     func create(employee: Employee) {
-        <#code#>
+        
+        let cdEmployee = CDEmployee(context: PersistentStorage.shared.context)
+        cdEmployee.email = employee.email
+        cdEmployee.name = employee.name
+        cdEmployee.id = employee.id
+        cdEmployee.profilePicture = employee.profilePicture
+        
+        PersistentStorage.shared.saveContext()
     }
     
     func getAll() -> [Employee]? {
-        <#code#>
+        
+        let result = PersistentStorage.shared.fetchManagedObject(managedObject: CDEmployee.self)
+        var employees = [Employee]()
+        result?.forEach({ cdEmployee in
+            employees.append(cdEmployee.convertToEmployee())
+        })
+        return employees
     }
-    func get(byIdentifier: UUID) -> Employee? {
-        <#code#>
+    func get(byIdentifier id: UUID) -> Employee? {
+        let fetchRequest = NSFetchRequest<CDEmployee>(entityName: "CDEmployee")
+        let predicate = NSPredicate(format: "id==%@", id as CVarArg)
+        
+        fetchRequest.predicate = predicate
+        do {
+            let result = try PersistentStorage.shared.context.fetch(fetchRequest).first
+            guard result != nil else { return nil }
+            return result?.convertToEmployee()
+        } catch {
+            debugPrint(error)
+        }
     }
     
     func update(employee: Employee) {
